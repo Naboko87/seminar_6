a2a62c7a489fc360e2fef1f1dca3e41d2660b5d7
Kseniya <Naboko.kv@gmail.com>
2022-12-04 02:39:25 +0200

    eee

--- ex_43/Program.cs
+++ ex_43/Program.cs
@@ -1,20 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, ex_43");
 Console.WriteLine("write b1");
-int b1 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("write b2");
-int b2 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("write k1");
-int k1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("write k2");
-int k2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
 
-primer(b1, b2, k1, k2);
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Прямые совпадают");
+} else if (k1 == k2)
+{
+   Console.WriteLine("Прямые параллельны"); 
+} else
+{
+    primer(b1, b2, k1, k2);
+}
 
-void primer(int a1, int a2, int c1, int c2){
-int y = 0;
-int x = 0;    
-x = (c2 - c1)/(a1 - a2);
-y = a1 * x + c1;
-Console.WriteLine(y);
+
+void primer(double bb1, double bb2, double kk1, double kk2){
+double y = 0;
+double x = 0;    
+Console.WriteLine("x:" +x);
+Console.WriteLine("y:" +y);
+Console.WriteLine("b1:" +bb1);
+Console.WriteLine("b2:" +bb2);
+Console.WriteLine("k1:" +kk1);
+Console.WriteLine("k2:" +kk2);
+
+x = (bb2 - bb1)/(kk1 - kk2);
+Console.WriteLine("x:" +x);
+
+y=(kk1*(bb2-bb1))/(kk1-kk2)+bb1;
+
+Console.WriteLine("y:" +y);
 }
